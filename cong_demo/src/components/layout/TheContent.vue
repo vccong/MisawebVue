fede7ed29405141d5852833fc7ae993d2b166dff
Công Vương Chí <copvogai@gmail.com>
2021-04-14 11:54:55 +0700

    thu4

---
+++ cong_demo/src/components/layout/TheContent.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+      <div class="content">
+          <div class="content-body">
+              <div class="content-first">
+                <div class="first-header">
+                    <h1>
+                          Nhân viên
+                    </h1>
+                    <div class="first-header-right">
+                        <button class="b-Utilities">
+                            <span>Tiện ích</span>
+                            <div class="b-Utilities-icon"></div>
+                        </button>
+                        <div class="b-add">
+                            <button class="b-add-1">
+                            <span>Thêm</span>
+                            </button>
+                            <button class="b-add-2">
+                                <div class="b-add-2-icon">
+
+                                 </div>  
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            <div class="first-content">
+                <div class="first-content-icon">
+
+                </div>
+                <a href="">Tất cả danh mục</a>
+            </div>
+            </div>
+          <div class="content-second">
+
+          </div>
+          <div class="content-third">
+
+          </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.content{
+    width: calc(100vw - 198px);
+    height: calc(100vh - 48px);
+    position: relative;
+    top: 48px;
+    left: 178px;
+    padding: 0 0 0 20px;
+}
+
+.content .content-body{
+    padding-right: 30px;
+    overflow: scroll;
+    height: calc(100vh - 48px);
+    position: absolute;
+    top: 0;
+    left: 20px;
+    right: 0;
+    bottom: 0;
+    background-color: #f4f5f6;
+}
+
+.content-body .content-first{
+    padding: 22px 0 16px;
+}
+
+.content-first .first-header{
+    display: flex;
+    justify-content: space-between;
+}
+
+.content-first .first-header h1{
+    margin: 0;
+}
+
+.content-first .first-header-right{
+    display: flex;
+}
+
+.content-first .first-header-right .b-Utilities{
+    height: 36px;
+    width: 103px;
+    border: 1px solid #8d9096;
+    border-radius: 30px;
+    box-sizing: border-box;
+    margin-right: 12px;
+    outline: none;
+    cursor: pointer;
+}
+
+.content-first .first-header-right .b-Utilities:hover{
+    background-color: #afb3b7;
+}
+
+.content-first .first-header-right .b-add{
+    display: flex;
+    width: 103px;
+    height: 36px;
+    box-sizing: border-box;
+    align-items: center;
+    
+}
+
+.first-header-right .b-add .b-add-1{
+    border-radius: 30px 0 0 30px;
+    padding: 8px 14px 8px 20px;
+    height: 36px;
+    border: 1px solid #8d9096;
+    background-color: #2ca01c;
+    border-right: 0;
+    outline: none;
+    cursor: pointer;
+    color: #ffffff;
+}
+
+.first-header-right .b-add .b-add-1:hover{
+    background-color: #3fd72d;
+}
+
+.first-header-right .b-add .b-add-2{
+    border-radius: 0 30px 30px 0;
+    padding: 8px 0 0 8px;
+    height: 36px;
+    border: 1px solid #8d9096;
+    background-color: #2ca01c;
+    outline: none;
+    cursor: pointer;
+}
+
+.first-header-right .b-add .b-add-2:hover{
+    background-color: #3fd72d;
+}
+
+.b-add-2 .b-add-2-icon{
+    background: url('../../assets/img/Sprites.svg')  no-repeat -848px -359px;
+    width: 24px;
+    height: 24px;
+}
+
+</style>
--- cong_demo/src/components/layout/TheHeader.vue
+++ cong_demo/src/components/layout/TheHeader.vue
@@ -5,13 +5,40 @@
             <div class="header-logo"></div>
             <div class="header-branch">
                 <div class="header-branch-title">CÔNG TNHH SÀI GÒN BAY</div>
-                <a class="header-branch-item"></a>
+                <a class="header-branch-item">
+                    <div></div>
+                </a>
             </div>
-            <div></div>
-            <div></div>
+            <div class="toltip-content">
+                <div class="toltip-icon"></div>
+                <div class="toltip-title">test_hieu_nang_db</div>
+            </div>
         </div>
         <div class="header-right">
-            
+            <div class="header-search">
+                <input type="text" name="" class="right-search" placeholder="Nhập từ khóa tìm kiếm">
+                <div class="right-icon"></div>
+            </div>   
+            <button class="right-item-1 b-click">
+                <div class="item-1-icon"></div>
+            </button> 
+            <button class="right-item-2 b-click">
+                <div class="item-2-icon"></div>
+            </button>
+            <button class="right-item-3 b-click">
+                <div class="item-3-icon"></div>
+            </button>
+            <button class="right-item-4 b-click">
+                <div class="item-4-icon"></div>
+            </button>
+            <button class="right-item-5 b-click">
+                <div class="item-5-icon"></div>   
+            </button>
+            <button class="right-item-6">
+                <div class="right-item-img"></div>
+                <div>Vương Chi Công</div>
+                <div class="right-item-icon"></div>
+            </button>
         </div>
     </div>
   </div>
@@ -45,9 +72,16 @@ export default {
     width: 24px;
     height: 24px;
     margin: 0 10px;
+    cursor: pointer;
 }
 
-.header-left .header-branch .header-branch-title{
+.header-left .header-branch{
+    margin: auto;
+    display: flex;
+    justify-content: center;
+}
+
+.header-branch .header-branch-title{
     padding-left: 10px;
     overflow: hidden;
     white-space: nowrap;
@@ -57,5 +91,143 @@ export default {
     transition: all .2s ease;
     font-size: 14px;
     color: #111;
+    cursor: pointer;
 }
+
+.header-branch .header-branch-item{
+    height: 21px;
+    width: 34px;
+    align-items: center;
+    cursor: pointer;
+}
+.header-branch .header-branch-item div{
+    width: 14px;
+    height: 14px;
+    background: url('../../assets/img/Sprites.svg') no-repeat -80px -360px;
+    transform: rotate(90deg);
+    margin: 2px 10px;
+}
+
+.header-left .toltip-content{
+    border: 1px solid #EBEDF0;
+    margin: 0 10px 0 15px;
+    padding: 0 12px;
+    border-radius: 40px;
+    height: 30px;
+    align-items: center;
+    box-sizing: border-box;
+    display: flex;
+    cursor: pointer;
+}
+
+.toltip-content .toltip-icon{
+    width: 8px;
+    height: 8px;
+    background-color: #4ea344;
+    border-radius: 50%;
+    border: 1px solid #fff;
+}
+
+.toltip-content .toltip-title{
+    margin-left: 7px;
+}
+
+.header .header-right{
+    display: flex;
+    align-items: center;
+    justify-items: center;
+}
+
+.header-right .header-search{
+    margin-right: 20px;
+}
+
+.header-right .header-search .right-search{
+    width: 220px;
+    height: 32px;
+    border: 1px solid #EBEDF0;
+    border-radius: 3px;
+    outline: none;
+}
+
+.header-right .header-search .right-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -992px -360px;
+    background-repeat: no-repeat;
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    top: 15px;
+    right: 450px;
+}
+
+.header-right .b-click{
+    height: 48px;
+    width: 44px;
+    background: #ffffff;
+    cursor: pointer;
+    padding: 0;
+    border: none;
+    outline: none;
+}
+
+.header-right .b-click .item-1-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -568px -30px;
+    height: 24px;
+    width: 24px;
+    margin: 0 10px;
+    
+}
+
+.header-right .b-click .item-2-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -675px -30px;
+    height: 24px;
+    width: 24px;
+    margin: 0 10px;
+}
+
+.header-right .b-click .item-3-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -899px -30px;
+    height: 24px;
+    width: 24px;
+    margin: 0 10px;
+}
+
+.header-right .b-click .item-4-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -731px -30px;
+    height: 24px;
+    width: 24px;
+    margin: 0 10px;
+}
+
+.header-right .b-click .item-5-icon{
+    background: url('../../assets/img/Sprites.svg') no-repeat -788px -30px;
+    height: 24px;
+    width: 24px;
+    margin: 0 10px;
+}
+
+.header-right .right-item-6{
+    padding: 0 16px;
+    display: flex;
+    background-color: #ffffff;
+    border: none;
+    cursor: pointer;
+    align-items: center;
+    outline: none;
+}
+
+
+.header-right .right-item-6 .right-item-img
+{
+    background: url('../../assets/img/Sprites.svg')  no-repeat -352px -894px;
+    height: 32px;
+    width: 32px;
+}
+.header-right .right-item-icon{
+    width: 14px;
+    height: 14px;
+    background: url('../../assets/img/Sprites.svg') no-repeat -1076px -32px;
+    margin: 2px 10px;
+}
+
 </style>
